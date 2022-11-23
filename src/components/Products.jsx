76518989efd4e5b9703e694b327b08547c422ca9
VinicinBraga <vinicius@bragamatta.com.br>
2022-11-23 16:44:49 -0300

    filterProduct

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -25,18 +26,63 @@ const Products = () => {
   }, []);
 
   const Loading = () => {
-    return <>Loading...</>;
+    return (
+      <>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+      </>
+    );
+  };
+
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((x) => x.category === cat);
+    setFilter(updatedList);
   };
 
   const ShowProducts = () => {
     return (
       <>
         <div className="buttons d-flex justify-content-center mb-5 pb-5">
-          <button className="btn btn-outline-dark me-2">All</button>
-          <button className="btn btn-outline-dark me-2">Men's Cloting</button>
-          <button className="btn btn-outline-dark me-2">Women's Cloting</button>
-          <button className="btn btn-outline-dark me-2">Jewelerys</button>
-          <button className="btn btn-outline-dark me-2">Eletronics</button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Cloting
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Cloting
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Jewelerys
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Eletronics
+          </button>
         </div>
         {filter.map((product) => {
           return (
@@ -53,18 +99,11 @@ const Products = () => {
                     <h4 className="card-title mb-0">
                       {product.title.substring(0, 14)}...
                     </h4>
-                    <p className="card-text lead fw-bold my-3">
+                    <p className="card-text lead fw-bold my-2">
                       ${product.price}
                     </p>
-                    <p className="lead fw-semibold">
-                      Rating: {product.rating.rate}
-                      <i className="fa fa-star"></i>
-                    </p>
-                    <p className="card-text lead">
-                      {product.description.substring(0, 40)}
-                    </p>
                     <button className="btn btn-outline-dark px-4 py-2">
-                      Add to Cart
+                      Buy Now
                     </button>
                   </div>
                 </div>
