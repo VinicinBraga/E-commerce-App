1aa5da614b430894933797fe4574a6310826555b
VinicinBraga <vinicius@bragamatta.com.br>
2022-11-25 14:33:35 -0300

    Create Product Component - Update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -15,7 +15,7 @@ const Product = () => {
       setLoading(true);
       const response = await fetch(`https://fakestoreapi.com/products/${id}`);
       setProduct(await response.json());
-      setLoading(true);
+      setLoading(false);
     };
     getProduct();
   }, []);
@@ -26,14 +26,13 @@ const Product = () => {
         <div className="col-md-6">
           <Skeleton height={400} />
         </div>
-        <div className="col-md-6" style={{ lineHeigth: 2 }}>
+        <div className="col-md-5" style={{ lineHeigth: 2 }}>
           <Skeleton height={50} width={300} />
           <Skeleton height={75} />
           <Skeleton height={25} width={150} />
           <Skeleton height={50} />
-          <Skeleton height={150} />
+          <Skeleton height={130} />
           <Skeleton height={50} width={100} />
-          <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
         </div>
       </>
     );
